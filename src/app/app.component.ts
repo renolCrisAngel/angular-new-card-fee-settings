bd6030488a1408e81ac27134a0e536ef163a5953
DESKTOP-C4IC0RN\Dev-Kej <karibas@intercardinc.com>
2022-05-19 13:14:55 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-new-card-fee-settings';
+
+  sellingprice: string = '';
+  pricenotax: string = '';
+  tax: string ='';
+  taxcategory: string = '';
+  lastmodified: string = '';
+  modifiedby: string = '';
+
+    constructor() {}
+    
+    ngOnInit(){
+
+    }
+
+    save() {
+    console.log("this is save button");
+    }
+
+    cancel() {
+    console.log("this is cancel button");
+    }
+
 }
+
+
+
